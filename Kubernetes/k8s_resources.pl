b934e1ef98b8d32e5d621793197a85be00d52645
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-17 14:51:39 +0100

    Update k8s_resources.pl

--- Kubernetes/k8s_resources.pl
+++ Kubernetes/k8s_resources.pl
@@ -9,7 +9,7 @@ foreach $resource (@resources)
 {
   my $command = 'kubectl get ' . $resource . ' --all-namespaces';
   print "############################################################################\n";
-  print color('bold lightblue');
+  print color('bold green');
   print "Getting resources of type: ";
   print color('reset');
   print color('bold blue');
