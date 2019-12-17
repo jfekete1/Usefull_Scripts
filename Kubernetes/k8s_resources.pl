103b9186d2197dd2fe82466ce2e9a197cf5a68be
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-17 14:29:07 +0100

    Get all Kubernetes resources in cluster.

---
+++ Kubernetes/k8s_resources.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+my $k8sResources = `kubectl api-resources | awk '{print \$1}'`;
+my @resources = split("\n", $k8sResources);
+shift @resources;
+
+foreach $resource (@resources)
+{
+  my $command = 'kubectl get ' . $resource . ' --all-namespaces';
+  print "############################################################################\n";
+  print "Getting resources of type: " . uc($resource) . "\n\s\s -with command: $command";
+  system($command);
+  print "############################################################################\n\n\n\n\n";
+}
