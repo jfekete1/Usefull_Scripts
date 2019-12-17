5bd86851373f795ff80375811084e7d5e3d974ca
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-17 21:37:30 +0100

    Update k8sHelper.pl

--- Kubernetes/k8sHelper.pl
+++ Kubernetes/k8sHelper.pl
@@ -26,6 +26,13 @@ given ($object) {
    when ($_ eq "service") {
        createDescription($_, "kubectl expose pod redis --port=6379 --name redis-service", "apiVersion: v1\nkind: Service\nmetadata:\n  labels:\n    run: redis\n  name: redis-service\nspec:\n  ports:\n  - port: 6379\n    protocol: TCP\n    targetPort: 6379\n  selector:\n    run: redis\n  sessionAffinity: None\n  type: ClusterIP\n");
    }
+   when ($_ eq "namespace"){
+       print color('bold green');
+       say "Setting namespace preference:";
+       print color('reset');
+       say "kubectl config current-context \nkubectl config set-context <here comes the current context name> --namespace=<insert-namespace-name-here>";
+       createDescription($_,"kubectl create namespace app-space","apiVersion: v1\nkind: Namespace\nmetadata:\n  name: app-space\n  selfLink: /api/v1/namespaces/app-space\nspec:\n  finalizers:\n  - kubernetes\nstatus:\n  phase: Active");
+   }
    default {
        print color('bold red');
        say "No information on object $_ !!";
