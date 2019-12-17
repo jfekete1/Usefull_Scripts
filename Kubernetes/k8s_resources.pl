651ad881f4f3b1c7972eee720f6298ff3f126fbb
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-17 14:48:16 +0100

    add coloring to output

--- Kubernetes/k8s_resources.pl
+++ Kubernetes/k8s_resources.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+use Term::ANSIColor;
+
+print color('bold blue');
+print "This text is bold blue.\n";
+print color('reset');
 
 my $k8sResources = `kubectl api-resources | awk '{print \$1}'`;
 my @resources = split("\n", $k8sResources);
@@ -8,7 +13,14 @@ foreach $resource (@resources)
 {
   my $command = 'kubectl get ' . $resource . ' --all-namespaces';
   print "############################################################################\n";
-  print "Getting resources of type: " . uc($resource) . "\n   -with command: $command \n";
+  print "Getting resources of type: ";
+  print color('bold blue');
+  print "" . uc($resource);
+  print color('reset');
+  print "\n   -with command: ";
+  print color('bold yellow');
+  print "$command \n";
+  print color('reset');
   system($command);
   print "############################################################################\n\n\n\n\n";
 }
