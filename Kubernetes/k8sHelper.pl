bc3da0cdd7730992d80eea3afbf9ebfff241c2a3
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-17 19:52:53 +0100

    Update k8sHelper.pl

--- Kubernetes/k8sHelper.pl
+++ Kubernetes/k8sHelper.pl
@@ -8,25 +8,15 @@ use Term::ANSIColor;
 my $object = "";
 if ($ARGV[0]){ $object = $ARGV[0];}
 
-sub imperativeText {
-   print color('bold blue');
-   say "Imperative $_ example:";
-   print color('reset');
-}
-sub declarativeText {
-   print color('bold yellow');
-   say "Declarative $_ example:";
-   print color('reset');
-}
 sub createDescription {
     print color('bold blue');
-    say "Imperative @_[0] example:";
+    say "Imperative $_[0] example:";
     print color('reset');
-    say @_[1];
+    say $_[1];
     print color('bold yellow');
-    say "Declarative $_ example:";
+    say "Declarative $_[0] example:";
     print color('reset');
-    say @_[2];
+    say $_[2];
 }
 
 given ($object) {
@@ -34,10 +24,7 @@ given ($object) {
        createDescription($_, "kubectl run --generator=run-pod/v1 nginx-pod --image=nginx:alpine\n", "apiVersion: v1 \nkind: Pod \nmetadata:\n  name: nginx-pod\nspec:\n  containers:\n  - name: nginx-container\n    image: nginx:alpine\n" );
    }
    when ($_ eq "service") {
-       imperativeText($_);
-       say "kubectl expose pod redis --port=6379 --name redis-service";
-       declarativeText($_);
-       say "apiVersion: v1\nkind: Service\nmetadata:\n  labels:\n    run: redis\n  name: redis-service\nspec:\n  ports:\n  - port: 6379\n    protocol: TCP\n    targetPort: 6379\n  selector:\n    run: redis\n  sessionAffinity: None\n  type: ClusterIP\n";
+       createDescription($_, "kubectl expose pod redis --port=6379 --name redis-service", "apiVersion: v1\nkind: Service\nmetadata:\n  labels:\n    run: redis\n  name: redis-service\nspec:\n  ports:\n  - port: 6379\n    protocol: TCP\n    targetPort: 6379\n  selector:\n    run: redis\n  sessionAffinity: None\n  type: ClusterIP\n");
    }
    default {
        print color('bold red');
