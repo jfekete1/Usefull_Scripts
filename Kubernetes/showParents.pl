ea4cd116282cfa80ac12f43d7d1534512decbf40
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-23 21:24:12 +0100

    updated to work properly

--- Kubernetes/showParents.pl
+++ Kubernetes/showParents.pl
@@ -10,6 +10,16 @@ my @linesArray;
 my @parentsArray;
 my $currentSpaceCount = 0;
 my $currentIndex = 0;
+my $initialIndex = 0;
+
+open my $info, $file or die "Could not open $file: $!";
+
+while( my $line = <$info>)  {   
+    push @linesArray, $line;
+	push @leadingSpaceArray, getLeadingSpaces($line);
+}
+
+close $info;
 
 sub getLeadingSpaces {
   my $str = $_[0];
@@ -18,53 +28,30 @@ sub getLeadingSpaces {
   return $spaceCount;
 }
 
-#find the needed line in file
-my $searchedLine = `grep -in $searchString $file | head -1`;
-
-#get the number of leading spaces and line number in found line
-my @searchedArray = split(/:/, $searchedLine, 2);
-$currentIndex = $searchedArray[0] - 1;
-my $initialIndex = $currentIndex;
-$currentSpaceCount = getLeadingSpaces($searchedArray[1]);
-#say $currentIndex;
-#say $currentSpaceCount;
-
-
-
-#search for lines with smaller amount of leading spaces
-#loop through the file and get the leading space count for all lines, save that to an array
-
-open my $info, $file or die "Could not open $file: $!";
-
-while( my $line = <$info>)  {
-    push @linesArray, $line;
-        push @leadingSpaceArray, getLeadingSpaces($line);
+my $searchedLines = `grep -nw $searchString $file`;
+my @searchLns = split(/\n/, $searchedLines);
+#say Dumper \@searchLns;
+foreach my $searchedLine (@searchLns) {
+  my @searchedArray = split(/:/, $searchedLine, 2);
+  $currentIndex = $searchedArray[0] - 1;
+  $initialIndex = $currentIndex;
+  $currentSpaceCount = getLeadingSpaces($searchedArray[1]);
+  getParentsRecursively();
+  print reverse(@parentsArray);
+  print $linesArray[$initialIndex];
+  @parentsArray = ();
 }
 
-close $info;
-#loop through that array, and find the biggest linenumbered line where the spacecount is 3 smaller than the searched line and the linenumber is smaller too.
-#foreach my $n (@leadingSpaceArray) {
-  #say $n;
-#}
 sub getParentsRecursively {
-for (my $i=scalar(@leadingSpaceArray); $i>0; $i--){
-  if($i < $currentIndex){
-    if($leadingSpaceArray[$i] < $currentSpaceCount){
-          push @parentsArray, $linesArray[$i];
-          $currentIndex = $i;
-          $currentSpaceCount = $leadingSpaceArray[$i];
-          getParentsRecursively();
-          last;
-        }
+  for (my $i=scalar(@leadingSpaceArray); $i>0; $i--){
+    if($i < $currentIndex){
+      if($leadingSpaceArray[$i] < $currentSpaceCount){
+	      push @parentsArray, $linesArray[$i];
+	      $currentIndex = $i;
+	      $currentSpaceCount = $leadingSpaceArray[$i];
+	      getParentsRecursively();
+	      last;
+	    }
+    }
   }
 }
-}
-getParentsRecursively();
-
-#save that linenumber and spacenumber
-#call the function recursively again, to find the line which has 3 less space and has the biggest line number smaller than the saved line
-#say Dumper \@parentsArray;
-print reverse(@parentsArray);
-print $linesArray[$initialIndex];
-#say Dumper \@linesArray;
-#say Dumper \@leadingSpaceArray;
