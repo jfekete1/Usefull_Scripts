7ef96be5f1c7ddd82234f4896cfb33ba9bc300d8
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2019-12-26 23:20:10 +0100

    added --withChildren option

--- Kubernetes/showParents.pl
+++ Kubernetes/showParents.pl
@@ -8,9 +8,11 @@ my $file = 'tmp.yml';
 my @leadingSpaceArray;
 my @linesArray;
 my @parentsArray;
+my @childrenArray;
 my $currentSpaceCount = 0;
 my $currentIndex = 0;
 my $initialIndex = 0;
+my $initialSpaceCount = 0;
 
 open my $info, $file or die "Could not open $file: $!";
 
@@ -30,29 +32,49 @@ sub getLeadingSpaces {
 
 my $searchedLines = `grep -nw $searchString $file`;
 my @searchLns = split(/\n/, $searchedLines);
-#say Dumper \@searchLns;
+
 foreach my $searchedLine (@searchLns) {
   my @searchedArray = split(/:/, $searchedLine, 2);
   $currentIndex = $searchedArray[0] - 1;
   $initialIndex = $currentIndex;
   $currentSpaceCount = getLeadingSpaces($searchedArray[1]);
+  $initialSpaceCount = $currentSpaceCount;
   getParentsRecursively();
   print reverse(@parentsArray);
   print $linesArray[$initialIndex];
+  if($ARGV[1]){
+    if($ARGV[1] eq "--withChildren" || $ARGV[1] eq "--withChilds" || $ARGV[1] eq "-wc"){
+      getChildren();
+	  foreach my $children (@childrenArray){
+	    print $children;
+	  }
+    }
+  }
   @parentsArray = ();
+  @childrenArray = ();
   say "\n";
 }
 
 sub getParentsRecursively {
-  for (my $i=scalar(@leadingSpaceArray); $i>0; $i--){
-    if($i < $currentIndex){
-      if($leadingSpaceArray[$i] < $currentSpaceCount){
-	      push @parentsArray, $linesArray[$i];
-	      $currentIndex = $i;
-	      $currentSpaceCount = $leadingSpaceArray[$i];
-	      getParentsRecursively();
-	      last;
-	    }
-    }
+for (my $i=scalar(@leadingSpaceArray); $i>0; $i--){
+  if($i < $currentIndex){
+    if($leadingSpaceArray[$i] < $currentSpaceCount){
+	  push @parentsArray, $linesArray[$i];
+	  $currentIndex = $i;
+	  $currentSpaceCount = $leadingSpaceArray[$i];
+	  getParentsRecursively();
+	  last;
+	}
+  }
+}
+}
+
+sub getChildren {
+  for(my $i=$initialIndex; $i<scalar(@leadingSpaceArray); $i++){
+    if($initialSpaceCount < $leadingSpaceArray[$i+1]){
+	  push @childrenArray, $linesArray[$i+1];
+	}else{
+	  last;
+	}
   }
 }
