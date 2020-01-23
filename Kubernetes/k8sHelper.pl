4d5f177b31362116b1f99469f95962be3c76f46c
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2020-01-23 14:21:05 +0100

    Update k8sHelper.pl

--- Kubernetes/k8sHelper.pl
+++ Kubernetes/k8sHelper.pl
@@ -122,7 +122,7 @@ given ($object) {
        say "spec:\n  affinity:\n    nodeAffinity:\n      requiredDuringSchedulingIgnoredDuringExecution:\n        nodeSelectorTerms:\n        - matchExpressions:\n          - key: app_type\n            operator: In\n            values:\n            - beta";
    }
    when ($_ eq "ingress") {
-       createDescription($_, "No imperative command available for this resource type !! \nkubectl explain ingress --recursive", "apiVersion: networking.k8s.io/v1beta1\nkind: Ingress\nmetadata:\n  name: test-ingress\n  annotations:\n    nginx.ingress.kubernetes.io/rewrite-target: /\nspec:\n  rules:\n  - http:\n      paths:\n      - path: /testpath\n        backend:\n          serviceName: test\n          servicePort: 80");
+       createDescription($_, "No imperative command available for this resource type !! \nkubectl explain ingress --recursive", "apiVersion: extensions/v1beta1\nkind: Ingress\nmetadata:\n  name: test-ingress\n  annotations:\n    nginx.ingress.kubernetes.io/rewrite-target: /\nspec:\n  rules:\n  - host: ckad-mock-exam-solution.com\nhttp:\n      paths:\n      - path: /video\n        backend:\n          serviceName: my-video-service\n          servicePort: 8080");
    }
    when ($_ eq "readinessprobe"){
        createDescription($_, "No imperative command available for this resource type !! \nkubectl explain pod --recursive | grep -i readiness -A10", "apiVersion: v1\nkind: Pod\nmetadata:\n  name: goproxy\n  labels:\n    app: goproxy\nspec:\n  containers:\n  - name: goproxy\n    image: k8s.gcr.io/goproxy:0.1\n    ports:\n    - containerPort: 8080\n    readinessProbe:\n      tcpSocket:\n        port: 8080\n      initialDelaySeconds: 5\n      periodSeconds: 10");
