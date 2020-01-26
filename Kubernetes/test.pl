ad23770a00df7cae795d97d0353c129a6c7fa7c1
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2020-01-26 09:27:25 +0100

    Update test.pl

--- Kubernetes/test.pl
+++ Kubernetes/test.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl
 use Net::SSH::Perl;
-
+use Config::Hosts;
+ 
+my $hosts = Config::Hosts->new();
+    $hosts->read_hosts(); # reads default /etc/hosts
+    $hosts->insert_host(ip => "8.8.8.8", hosts => [qw(host1 host2)]);
+    $hosts->write_hosts("/tmp/hosts");
+    
 my $nodeIP='192.168.0.129';
 my $username = 'osboxes';
 my $password = 'osboxes.org';
