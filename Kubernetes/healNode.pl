44429728f7612c51b99a6c693a18d7fa8e562b7e
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2020-02-05 10:58:22 +0100

    Update healNode.pl

--- Kubernetes/healNode.pl
+++ Kubernetes/healNode.pl
@@ -3,8 +3,11 @@
 my $masterIP = $ARGV[0];
 my $nodeIP   = $ARGV[1];
 
-`sed -r \"s/^\\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(\\s+master)/$masterIP\\1/\"`;
-`sed -r \"s/^\\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(\\s+master)/$nodeIP\\1/\"`;
+my $newHosts = `cat /etc/hosts | sed -r \"s/^\\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(\\s+master)/$masterIP\\1/\"`;
+`echo \"$newHosts\" > /etc/hosts`;
+$newHosts = `cat /etc/hosts | sed -r \"s/^\\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(\\s+node01)/$nodeIP\\1/\"`;
+`echo \"$newHosts\" > /etc/hosts`;
+
 `systemctl stop docker`;
 `rm -rf /var/lib/cni`;
 `systemctl start docker`;
