acfa40c1bd703bf00587d5c0aef7d482331a1d46
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2020-10-15 19:59:12 +0200

    Update setEnv.sh

--- setEnv.sh
+++ setEnv.sh
@@ -9,4 +9,4 @@ cp k8help.pl /usr/local/bin/k8help.pl
 alias kcr="kubectl create -f"
 alias secret="kubectl create secret generic mysql-pass --from-literal=password=admin --dry-run -o yaml > secret.yml"
 alias deployment="kubectl create deployment mysql --image=mysql:5.7 --dry-run -o yaml > deployment.yml"
-alias service_nodeport="while read line do kubectl expose deployment $line --name=webapp-service --target-port=8080 --type=NodePort --port=8080 --dry-run -o yaml > service-nodeport-webapp.yml done"
+alias service_nodeport="while read line; do kubectl expose deployment $line --name=$line --target-port=8080 --type=NodePort --port=8080 --dry-run -o yaml > service-nodeport-webapp.yml; done"
