1d5d2d141296d6f69172c0979a6e110da1564490
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2020-10-15 18:59:26 +0200

    Update setEnv.sh

--- setEnv.sh
+++ setEnv.sh
@@ -2,6 +2,6 @@
 export RAW_DEF=https://raw.githubusercontent.com/jfekete1/Usefull_Scripts/master/Kubernetes/Definitions
 export TMP_DEF=${1:-deployment}
 curl $RAW_DEF/kubeStructureInfo.pl > k8help.pl
-chmod 777 help.pl
+chmod 777 k8help.pl
 kubectl explain $TMP_DEF --recursive > /tmp/tmp.yml
 cp k8help.pl /usr/local/bin/k8help.pl
